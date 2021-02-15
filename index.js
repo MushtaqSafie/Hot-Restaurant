1a031bb8bb321755ef4d2a79980534ae42da0205
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-02-15 20:30:55 +1100

    make it better

--- index.js
+++
@@ -1,9 +0,0 @@
-// need to create the following pages front-end
-// home-page.html ./
-// reserve-page.html ./table
-// tables-page.html ./reserve
-
-
-// Json object includes {name: phoneNumber: email: uniqueID:}
-
-// 2 API is requier /api/tables , /api/waitlist 
